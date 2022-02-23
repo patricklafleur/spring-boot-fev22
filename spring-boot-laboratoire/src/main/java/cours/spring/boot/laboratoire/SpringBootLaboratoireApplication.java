c475f99e35eb672a46a08b328c286e843e876eaf
patrick.lafleur@creationobjet.com <patrick.lafleur@creationobjet.com>
2022-02-23 13:26:04 -0500

    commit initial

---
+++ spring-boot-laboratoire/src/main/java/cours/spring/boot/laboratoire/SpringBootLaboratoireApplication.java
@@ -0,0 +1,13 @@
+package cours.spring.boot.laboratoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Classe principale
+public class SpringBootLaboratoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLaboratoireApplication.class, args);
+	}
+
+}
---
+++ spring-boot-laboratoire/src/test/java/cours/spring/boot/laboratoire/SpringBootLaboratoireApplicationTests.java
@@ -0,0 +1,14 @@
+package cours.spring.boot.laboratoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringBootLaboratoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
